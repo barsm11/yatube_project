27e385b257bde01f4c0999ac3d86159785800a34
Александр Марханов <markhanov-an@yandex.ru>
2022-05-05 19:29:03 +0700

    Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Что-то интересное {slug}')
+
+# Create your views here.
